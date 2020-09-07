6721db6ccb5b77d4c0b8a43c5d2afb20a3964eef
martin.reinhardt <martin.reinhardt@iwelt.de>
2020-09-07 15:41:47 +0200

    adding test case for converting instance of product (case class) to map

--- src/test/scala/com/sfxcode/templating/pebble/PebbleEngineSpec.scala
+++ src/test/scala/com/sfxcode/templating/pebble/PebbleEngineSpec.scala
@@ -14,6 +14,31 @@ class PebbleEngineSpec extends Specification {
       evaluated must contain("<title>pebble-scala</title>")
       evaluated must contain("<li>Element1</li>")
     }
+
+    "evaluate template from string and case class from context" in {
+
+      val someBigLongValue   : Long   = 99999L
+      val otherLongValue     : Long   = 6666L
+      val niceDoubleValue    : Double =  25.25
+      val aDescriptionString : String = "Das ist ein einfacher String"
+
+      val data = TestCaseClass(someBigLongValue, otherLongValue, niceDoubleValue, aDescriptionString)
+      val context = Map("data" -> data)
+      val evaluated = Engine.evaluateToString("templates/engine/basic_injection.peb", context)
+      evaluated must contain("Das ist ein einfacher String")
+    }
+
   }
 
 }
+
+/**
+ * Simple Helper Case Class for Testing injection of case classes into pebble templates
+ * The implementation is not main part of test case
+ *
+ * @param someBigLongValue - just a long value
+ * @param otherLongValue - just another long value
+ * @param niceDoubleValue - just a double value
+ * @param aDescriptionString - just a String value
+ */
+case class TestCaseClass(someBigLongValue: Long, otherLongValue: Long, niceDoubleValue: Double, aDescriptionString: String)
