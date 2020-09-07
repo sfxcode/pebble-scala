8bd6f8756a05ef37a04018750332c5e71ef4b554
martin.reinhardt <martin.reinhardt@iwelt.de>
2020-09-07 15:19:05 +0200

    converting instance of product (case class) to map and call ResolvedAttribute

--- src/main/scala/com/sfxcode/templating/pebble/extension/ScalaResolver.scala
+++ src/main/scala/com/sfxcode/templating/pebble/extension/ScalaResolver.scala
@@ -6,23 +6,25 @@ import com.mitchellbosecke.pebble.template.EvaluationContextImpl
 
 case class ScalaResolver() extends AttributeResolver {
   override def resolve(
-      obj: Any,
-      attributeNameValue: Any,
-      argumentValues: Array[AnyRef],
-      args: ArgumentsNode,
-      context: EvaluationContextImpl,
-      filename: String,
-      lineNumber: Int
-  ): ResolvedAttribute =
+                        obj: Any,
+                        attributeNameValue: Any,
+                        argumentValues: Array[AnyRef],
+                        args: ArgumentsNode,
+                        context: EvaluationContextImpl,
+                        filename: String,
+                        lineNumber: Int
+                      ): ResolvedAttribute =
     obj match {
 
       case map: collection.Map[String, _] if map.keySet.contains(attributeNameValue.toString) =>
         new ResolvedAttribute(map(attributeNameValue.toString))
       case it: collection.Iterable[_]
-          if !it.isInstanceOf[Map[String, Any]] && it.size > attributeNameValue.toString.toInt =>
+        if !it.isInstanceOf[Map[String, Any]] && it.size > attributeNameValue.toString.toInt =>
         new ResolvedAttribute(it.toList(attributeNameValue.toString.toInt))
       case option: Option[_] if option.isDefined =>
         new ResolvedAttribute(option.get)
+      case anyObj: Any if anyObj.isInstanceOf[Product] =>
+        new ResolvedAttribute(anyObj.getClass.getDeclaredFields.map(_.getName).zip(anyObj.asInstanceOf[Product].productIterator.toSeq).toMap)
       case _ => null
     }
 
